102779cde5283cfd53ad8281628a3012ddafa2cd
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-06-08 14:29:36 -0400

    chat test

---
+++ script.js
@@ -0,0 +1,6 @@
+/*function writeData() {
+    firebase.database().ref("User").set({
+        name: document.getElementById("namefield").value,
+        message: document.getElementById("messagefield").value
+    });
+}   console.log("Working");*/
