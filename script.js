6b9900f243a700c01d266d89e9d6f4a02e092616
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2023-03-29 21:27:25 -0400

    ev

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function myFunction() {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
@@ -22,20 +20,21 @@ function myFunction() {
   }
 }
 
-var liText = '', liList = $('#myidd li'), listForRemove = [];
+var liText = '',
+  liList = $('#myidd li'),
+  listForRemove = [];
 
 $(liList).each(function () {
-    
+
   var text = $(this).text();
 
-  if (liText.indexOf('|'+ text + '|') == -1)
-    liText += '|'+ text + '|';
+  if (liText.indexOf('|' + text + '|') == -1)
+    liText += '|' + text + '|';
   else
     listForRemove.push($(this));
-    
+
 });
-    
-$(listForRemove).each(function () { $(this).remove(); });
 
-
-
+$(listForRemove).each(function () {
+  $(this).remove();
+});
